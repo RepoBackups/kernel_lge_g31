5ff0c81afdaed4dac3338d4057865f5eeb6236af
TripNRaVeR <noerialexeew@live.nl>
2014-01-14 03:01:54 +0100

    TripNDroid CPU Governor
    Signed-off-by: TripNRaVeR <noerialexeew@live.nl>
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -446,6 +446,9 @@ extern struct cpufreq_governor cpufreq_gov_intellimm;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PRESERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_preservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_preservative)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_TRIPNDROID)
+extern struct cpufreq_governor cpufreq_gov_tripndroid;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_tripndroid)
 #endif
 
 
